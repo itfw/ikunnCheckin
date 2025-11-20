88e9cc9b7d932b6504d4a96684d7359705ef31e8
kc <kc@Chenjingliangs-MacBook.local>
2025-11-20 09:10:33 +0800

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# 怎么使用 ikunnCheckin
+
+## 1.fork此项目
+
+## 2.配置仓库变量
+### 在setting设置里找到Secrets and variables下的 Actions
+![alt text](./imgs/setting.png)
+### 格式
+   1. **密钥名为：IKUUU_ACCOUNTS** (注意此数据结构为json,请不要尾随逗号,为确保格式正确请在ide里书写好json数据再复制到此处)
+  ```json
+  [
+    { "email": "your_email_1","password": "your_password_1" },
+    { "email": "your_email_2","password": "your_password_2" }
+    // ...
+  ]
+  ```
+   2. **密钥名为：IKUUU_API_PATH**
+  ```plain
+   https://ikuuu.de
+  ```
+### 配置完毕后记得首次启动一下action
+![alt text](./imgs/action.png)
+
+## 愉快的使用‘坐享其成’吧
---
+++ livesub.js
@@ -0,0 +1,48 @@
+const https = require('https');
+const fs = require('fs');
+function fetchPage(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, {
+      headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36' }
+    }, (res) => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => resolve(data));
+    }).on('error', reject);
+  });
+}
+
+function extractNuxtData(html) {
+  const match = html.match(/<script[^>]*id="__NUXT_DATA__"[^>]*>(.*?)<\/script>/s);
+  if (!match) throw new Error('__NUXT_DATA__ not found');
+  return JSON.parse(match[1]);
+}
+
+async function main() {
+  console.log('正在获取数据...');
+  baseURL = process.env.API1_PATH
+  const html = await fetchPage(baseURL);
+
+  console.log('解析数据');
+  const nuxt = extractNuxtData(html);
+  const base64Str = nuxt[34];
+  if (typeof base64Str !== 'string') throw new Error('字段无效');
+  const decoded = Buffer.from(base64Str.replace(/\n/g, ''), 'base64').toString('utf8');
+
+  console.log('过滤中...');
+  const filtered = decoded
+    .split('\n')
+    .filter(line => !line.toLowerCase().includes('hysteria2'))
+    .join('\n');
+
+  console.log('重新编码');
+  const newBase64 = Buffer.from(filtered, 'utf8').toString('base64');
+  console.log(newBase64)
+  fs.writeFileSync('live', newBase64 + '\n');
+  console.log('已完成');
+}
+
+main().catch(err => {
+  console.error('错误:', err.message);
+  process.exit(1);
+});
